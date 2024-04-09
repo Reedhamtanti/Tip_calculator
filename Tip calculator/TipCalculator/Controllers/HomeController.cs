efd551127d6e99c29e9ba0a66ec02926ae8134ca
Reedhamtanti <reedham.tanti3@gmail.com>
2024-04-09 18:36:43 -0400

    Tip_calculator

---
+++ Tip calculator/TipCalculator/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿    using Microsoft.AspNetCore.Mvc;
+using TipCalculator.Models;
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Nineteen = 0;
+            ViewBag.Fiftynine = 0;
+            ViewBag.Ninetythree = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calculator calc)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Nineteen = calc.CalculateTip(0.19);
+                ViewBag.Fiftynine = calc.CalculateTip(0.59);
+                ViewBag.Ninetythree = calc.CalculateTip(0.93);
+            }
+            else
+            {
+                ViewBag.Nineteen = 0;
+                ViewBag.Fiftynine = 0;
+                ViewBag.Ninetythree = 0;
+            }
+            return View(calc);
+        }
+    }
+}
